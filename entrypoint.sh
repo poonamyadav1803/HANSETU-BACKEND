10ccfc5f7abbf67afcfe0b55beda3f2abbcbc29b
sunidhi pandey <157216398+sunidhi12pandey@users.noreply.github.com>
2026-05-02 00:23:19 +0530

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-echo "Running DB migrations..."
+echo "Running migrations..."
 
 node dist/db/migrations/create_new_tables.js
 node dist/db/migrations/create_service_tables.js
 node dist/db/migrations/create_gst_table.js
 
-echo "Running DB seed..."
-npm run db:seed
+echo "Running seed..."
+node dist/db/seeds/index.js
 
 echo "Starting server..."
 exec node dist/server.js
